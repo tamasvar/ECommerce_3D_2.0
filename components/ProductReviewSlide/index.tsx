4441bcb2022a4261c8de4f37f7804d6a4d61b17c
Muhammad Shoaib <m.xhoaib1@gmail.com>
2024-07-07 14:46:47 +0500

    carousel style improvement

--- components/ProductReviewSlide/index.tsx
+++ components/ProductReviewSlide/index.tsx
@@ -4,11 +4,13 @@ import UserReview from '../UserReview'
 const ProductReviewSlide: FC<{ review: any, children: ReactNode }> = ({ review, children }) => {
   return (
     <>
-      <div className='mb-24'>
+      <div className='mb-20'>
         {children}
       </div>
-      <p className='mb-4 ml-[52px] text-xl font-medium'>{review?.product?.name}</p>
-      <UserReview review={review} />
+      <div className='border-t pt-4'>
+        <p className='mb-4 ml-[52px] text-xl font-medium'>{review?.product?.name}</p>
+        <UserReview review={review} />
+      </div>
     </>
   )
 }
