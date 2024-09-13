9904a791674f6e4d407be220ed1e45e9ba7f3727
tamasvar <vardai.tamas66@gmail.com>
2024-09-13 20:27:02 +0200

    Update index.tsx

--- components/CartSummary/index.tsx
+++ components/CartSummary/index.tsx
@@ -399,7 +399,7 @@ export function CartSummary() {
       setFormData(userData?.shippingAddress);
       setFormattedAddress(getAddressString(userData?.shippingAddress));
     }
-  }, [userData]);
+  }, [userData,cartDetails]);
 
   useEffect(() => {
     validateForm();
